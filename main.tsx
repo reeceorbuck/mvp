cea95aa30c975fa839633958f04137b7b0ab6492
reeceorbuck <reeceorbuck@me.com>
2026-03-14 22:30:45 +0800

    initial commit

---
+++ main.tsx
@@ -0,0 +1,56 @@
+import { Hono } from "hono";
+import { addTinyTools, ClientTools, css } from "@tiny-tools/hono";
+import { buildScriptFiles } from "@tiny-tools/hono/build";
+
+const clientTools = new ClientTools(import.meta.url, {
+  functions: {
+    clickHandler: function (_e: Event) {
+      console.log("clicked");
+    },
+  },
+  styles: {
+    buttonStyle: css`
+      background: #1eff00;
+      color: white;
+    `,
+  },
+});
+
+const testTools = new ClientTools(import.meta.url, {
+  functions: {
+    clickHandler: function (_e: Event) {
+      console.log("clicked alt test");
+    },
+  },
+  styles: {
+    buttonStyle: css`
+      background: orange;
+      color: white;
+    `,
+  },
+});
+
+const app = new Hono()
+  .use(...addTinyTools());
+
+app.get("/", (c) => {
+  const { fn, styled } = clientTools.engage();
+  return c.render(
+    <button type="button" class={styled.buttonStyle} onClick={fn.clickHandler}>
+      Hello World!
+    </button>,
+  );
+});
+
+app.get("/alt", (c) => {
+  const { fn, styled } = testTools.engage();
+  return c.render(
+    <button type="button" class={styled.buttonStyle} onClick={fn.clickHandler}>
+      Hello World!
+    </button>,
+  );
+});
+
+await buildScriptFiles();
+
+Deno.serve({ port: 3032 }, app.fetch);
